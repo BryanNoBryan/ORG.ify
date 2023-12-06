34bfc3a5cdd953e088de28581fe360ff8819d9f1
BryanNoBryan <121914672+BryanNoBryan@users.noreply.github.com>
2023-12-05 23:39:57 -0500

    gorouter is actually so nice

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:midyear/home_page.dart';
+import 'package:go_router/go_router.dart';
+import 'package:midyear/HomePage.dart';
+import 'package:midyear/admin/AdminHome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: GoRouter(
+        initialLocation: '/',
+        routes: [
+          GoRoute(
+            name:
+                'home', // Optional, add name to your routes. Allows you navigate by name instead of path
+            path: '/',
+            builder: (context, state) => HomePage(),
+          ),
+          GoRoute(
+            name: 'page2',
+            path: '/page2',
+            builder: (context, state) => AdminHome(),
+          ),
+        ],
+      ),
       title: 'Midyear App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      // home: const HomePage(),
     );
   }
 }
